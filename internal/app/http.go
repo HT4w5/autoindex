f5d8a981c7d3c28d079986e46b6ab33617bfdff8
HT4w5 <ht4w5x@gmail.com>
2026-02-26 18:33:22 +0800

    fix(app): return json when path not found

--- internal/app/http.go
+++ internal/app/http.go
@@ -6,10 +6,16 @@ const (
 	contentTypeJSON = "application/json"
 )
 
+var (
+	bodyNotFound = []byte(`{"code":404}`)
+)
+
 func (app *Application) HandleQuery(ctx *fasthttp.RequestCtx) {
 	resp, ok := app.index.QueryBytes(string(ctx.Path()))
 	if !ok {
-		ctx.NotFound()
+		ctx.SetContentType(contentTypeJSON)
+		ctx.SetStatusCode(fasthttp.StatusNotFound)
+		ctx.SetBody(bodyNotFound)
 		return
 	}
 

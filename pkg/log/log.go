73dcbc8dc90acc5d5888efe06de7522e19f731c4
HT4w5 <ht4w5x@gmail.com>
2026-02-26 23:37:13 +0800

    feat(log): improve logging

--- pkg/log/log.go
+++ pkg/log/log.go
@@ -1,6 +1,12 @@
 package log
 
-import "log"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/valyala/bytebufferpool"
+)
 
 type Logger interface {
 	Debugf(format string, a ...any)
@@ -19,32 +25,51 @@ const (
 	Debug
 )
 
+var (
+	errorTagBytes = []byte(" [ERROR] ")
+	warnTagBytes  = []byte(" [WARN] ")
+	infoTagBytes  = []byte(" [INFO] ")
+	debugTagBytes = []byte(" [DEBUG] ")
+)
+
 type SimpleLogger struct {
 	Level LogLevel
 }
 
 func (l *SimpleLogger) Debugf(format string, a ...any) {
 	if l.Level >= Debug {
-		log.Printf("[DEBUG] "+format, a...)
+		logf(debugTagBytes, format, a...)
 	}
 }
 
 func (l *SimpleLogger) Infof(format string, a ...any) {
 	if l.Level >= Info {
-		log.Printf("[INFO] "+format, a...)
+		logf(infoTagBytes, format, a...)
 	}
 }
 
 func (l *SimpleLogger) Warnf(format string, a ...any) {
 	if l.Level >= Warn {
-		log.Printf("[WARN] "+format, a...)
+		logf(warnTagBytes, format, a...)
 	}
 }
 
 func (l *SimpleLogger) Errorf(format string, a ...any) {
 	if l.Level >= Error {
-		log.Printf("[ERROR] "+format, a...)
+		logf(errorTagBytes, format, a...)
 	}
+}
+
+func logf(levelTag []byte, format string, a ...any) {
+	bb := bytebufferpool.Get()
+	defer bytebufferpool.Put(bb)
+
+	bb.WriteString(time.Now().Format(time.RFC3339))
+	bb.Write(levelTag)
+	fmt.Fprintf(bb, format, a...)
+	bb.WriteByte('\n')
+
+	bb.WriteTo(os.Stdout)
 }
 
 type DiscardLogger struct {

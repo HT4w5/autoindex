e9d67d5f047deda77c3b89d764c8f589fa0d71c7
HT4w5 <ht4w5x@gmail.com>
2026-02-27 12:58:04 +0800

    testing(index): add tests

---
+++ pkg/index/index_test.go
@@ -0,0 +1,301 @@
+package index_test
+
+import (
+	"bytes"
+	"encoding/binary"
+	"encoding/hex"
+	"fmt"
+	"math/rand"
+	"os"
+	"path/filepath"
+	"testing"
+	"time"
+
+	"github.com/HT4w5/autoindex/pkg/index"
+)
+
+const (
+	maxFileSize     = 1024
+	maxEntryCount   = 1024
+	randomTestSteps = 128
+)
+
+func writeContentMap(t *testing.T, dir string, contentMap map[string]index.Entry) {
+	for _, v := range contentMap {
+		if v.Type == index.TypeDir {
+			d := filepath.Join(dir, v.Name)
+			err := os.Mkdir(d, 0600)
+			if err != nil {
+				t.Fatalf("mkdir error: %v", err)
+			}
+			info, err := os.Stat(d)
+			if err != nil {
+				t.Fatalf("stat error: %v", err)
+			}
+			v.MTime = info.ModTime().Unix()
+			contentMap[v.Name] = v
+		} else {
+			f, err := os.Create(filepath.Join(dir, v.Name))
+			if err != nil {
+				t.Fatalf("create error: %v", err)
+			}
+			err = f.Truncate(v.Size)
+			if err != nil {
+				t.Fatalf("truncate error: %v", err)
+			}
+			info, err := f.Stat()
+			if err != nil {
+				t.Fatalf("stat error: %v", err)
+			}
+			v.MTime = info.ModTime().Unix()
+			contentMap[v.Name] = v
+		}
+	}
+}
+
+// Create temporary directory with random files and index.Response representing it
+func makeRandomDir(r *rand.Rand, t *testing.T) (string, index.Response) {
+	// Generate response first
+	resp := index.Response{
+		Type: index.TypeDir,
+	}
+
+	contentMap := make(map[string]index.Entry)
+
+	n := r.Intn(maxEntryCount)
+
+	for range n {
+		e := makeEntry(r)
+		contentMap[e.Name] = e
+	}
+
+	// Write to filesystem
+	dir := t.TempDir()
+	writeContentMap(t, dir, contentMap)
+
+	resp.Contents = make([]index.Entry, 0, len(contentMap))
+	for _, v := range contentMap {
+		resp.Contents = append(resp.Contents, v)
+	}
+
+	return dir, resp
+}
+
+func makeEntry(r *rand.Rand) index.Entry {
+	var buf bytes.Buffer
+	binary.Write(&buf, binary.BigEndian, r.Int63())
+	en := index.Entry{
+		Name: hex.EncodeToString(buf.Bytes()),
+	}
+	if r.Intn(2) == 0 {
+		// Create directory
+		en.Type = index.TypeDir
+	} else {
+		// Create file
+		en.Type = index.TypeFile
+		en.Size = r.Int63n(maxFileSize)
+	}
+	return en
+}
+
+func entryEq(t *testing.T, exp index.Entry, got index.Entry) {
+	if exp.Name != got.Name {
+		t.Error(errMsg("name", exp.Name, got.Name))
+	}
+	if exp.Type != got.Type {
+		t.Error(errMsg("type", exp.Type, got.Type))
+	} else {
+		if exp.Type == index.TypeFile {
+			if exp.Size != got.Size {
+				t.Error(errMsg("size", exp.Size, got.Size))
+			}
+		}
+	}
+	if exp.MTime != got.MTime {
+		t.Error(errMsg("mtime", exp.MTime, got.MTime))
+	}
+}
+
+func errMsg(name string, exp any, got any) string {
+	return fmt.Sprintf(
+		"%s mismatch: expected %v, got %v",
+		name,
+		exp,
+		got,
+	)
+}
+
+func TestRandomDirQuery(t *testing.T) {
+	for i := range randomTestSteps {
+		r := rand.New(rand.NewSource(int64(i)))
+		test := func(t *testing.T) {
+			dir, respExp := makeRandomDir(r, t)
+			idx, err := index.New(
+				index.WithRoot(dir),
+			)
+			if err != nil {
+				t.Fatalf("error creating index: %v", err)
+			}
+			respGot, ok := idx.Query("")
+			if !ok {
+				t.Error("index query failed")
+				return
+			}
+
+			if respGot.Type != index.TypeDir {
+				t.Error(errMsg("resp.Type", index.TypeDir, respGot.Type))
+			}
+
+			// Create got map
+			gotMap := make(map[string]index.Entry, 0)
+			for _, v := range respGot.Contents {
+				gotMap[v.Name] = v
+			}
+
+			for _, exp := range respExp.Contents {
+				got, ok := gotMap[exp.Name]
+				if !ok {
+					t.Errorf("missing entry %s", exp.Name)
+					continue
+				}
+
+				entryEq(t, exp, got)
+			}
+
+			if len(respExp.Contents) != len(respGot.Contents) {
+				t.Error(errMsg("content length", len(respExp.Contents), len(respGot.Contents)))
+			}
+
+			// Query again to test cache
+			respGot, ok = idx.Query("")
+			if !ok {
+				t.Error("index query failed")
+				return
+			}
+
+			if respGot.Type != index.TypeDir {
+				t.Error(errMsg("resp.Type", index.TypeDir, respGot.Type))
+			}
+
+			// Create got map
+			gotMap = make(map[string]index.Entry, 0)
+			for _, v := range respGot.Contents {
+				gotMap[v.Name] = v
+			}
+
+			for _, exp := range respExp.Contents {
+				got, ok := gotMap[exp.Name]
+				if !ok {
+					t.Errorf("missing entry %s", exp.Name)
+					continue
+				}
+
+				entryEq(t, exp, got)
+			}
+
+			if len(respExp.Contents) != len(respGot.Contents) {
+				t.Error(errMsg("content length", len(respExp.Contents), len(respGot.Contents)))
+			}
+		}
+		test(t)
+	}
+}
+
+func TestQueryFile(t *testing.T) {
+	content := map[string]index.Entry{
+		"file.dat": {
+			Name: "file.dat",
+			Size: 1024,
+			Type: index.TypeFile,
+		},
+	}
+
+	dir := t.TempDir()
+	writeContentMap(t, dir, content)
+
+	idx, err := index.New(
+		index.WithRoot(dir),
+	)
+	if err != nil {
+		t.Fatalf("error creating index: %v", err)
+	}
+
+	respGot, ok := idx.Query("/file.dat")
+	if !ok {
+		t.Error("index query failed")
+		return
+	}
+
+	if respGot.Type != "file" {
+		t.Error(errMsg("type", "file", respGot.Type))
+	}
+	if respGot.Size != 1024 {
+		t.Error(errMsg("type", "file", respGot.Type))
+	}
+
+	respGot, ok = idx.Query("/file.dat")
+	if !ok {
+		t.Error("index query failed")
+		return
+	}
+
+	if respGot.Type != "file" {
+		t.Error(errMsg("type", "file", respGot.Type))
+	}
+	if respGot.Size != 1024 {
+		t.Error(errMsg("type", "file", respGot.Type))
+	}
+}
+
+const (
+	minTestTTL = 1 * time.Second
+	maxTestTTL = 20 * time.Second
+)
+
+func TestCacheExpiry(t *testing.T) {
+	for ttl := minTestTTL; ttl <= maxTestTTL; ttl += time.Second {
+		t.Run(
+			fmt.Sprintf("TestCacheExpiry_%s_TTL", ttl.String()),
+			func(t *testing.T) {
+				t.Parallel()
+				content := map[string]index.Entry{
+					"file.dat": {
+						Name: "file.dat",
+						Size: 1024,
+						Type: index.TypeFile,
+					},
+				}
+
+				dir := t.TempDir()
+				writeContentMap(t, dir, content)
+
+				idx, err := index.New(
+					index.WithRoot(dir),
+					index.WithTTL(0),
+				)
+				if err != nil {
+					t.Fatalf("error creating index: %v", err)
+				}
+
+				_, ok := idx.Query("/file.dat")
+				if !ok {
+					t.Error("index query failed")
+					return
+				}
+
+				err = os.Remove(filepath.Join(dir, "file.dat"))
+				if err != nil {
+					t.Fatalf("remove error: %v", err)
+				}
+
+				<-time.After(ttl)
+
+				_, ok = idx.Query("/file.dat")
+				if ok {
+					t.Error("cache not expired")
+					return
+				}
+			},
+		)
+	}
+}
